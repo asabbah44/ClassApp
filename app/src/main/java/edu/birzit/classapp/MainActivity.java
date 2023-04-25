57acc4b4d2ac8fb02affa0481742bbf1aa9d9595
asabbah44 <43098459+asabbah44@users.noreply.github.com>
2023-04-25 16:22:50 +0300

    Spinner example

--- app/src/main/java/edu/birzit/classapp/MainActivity.java
+++ app/src/main/java/edu/birzit/classapp/MainActivity.java
@@ -2,13 +2,44 @@ package edu.birzit.classapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView l;
+    Button b;
+
+    Spinner spin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l=findViewById(R.id.lbl1);
+        b=(Button) findViewById(R.id.btn1);
+        spin=findViewById(R.id.spinner);
+
+    }
+
+    public void ShowMag(View view) {
+        String [] datalist={"Male","Female","soso"};
+
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,datalist);
+
+        spin.setAdapter(ad);
+
+
+        String gender= spin.getSelectedItem().toString();
+        l.setText(gender);
     }
 }
